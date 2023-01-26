d69af0abb68e68d1d57ffdce6e8e3bfed333ddd6
Muxiddin86 <imamberdievmuxiddin@gmail.com>
2023-01-26 15:12:48 +0500

    csotl

--- create_some_objects_to_list.py
+++ create_some_objects_to_list.py
@@ -5,3 +5,6 @@ from create_one_attribute import Person
 #Create an object named "p3" whose name is "Jasur"
 
 #Add these objects to the "persons" named list
+
+x = Person(["Anvar","Shavkat","Jasur"])
+print(x.name)
