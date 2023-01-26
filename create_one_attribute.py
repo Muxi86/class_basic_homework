167b4de76b2f27bc33db21bb71dcc9fff15d888c
Muxiddin86 <imamberdievmuxiddin@gmail.com>
2023-01-26 15:36:25 +0500

    coa

--- create_one_attribute.py
+++ create_one_attribute.py
@@ -1,6 +1,9 @@
 #Create a "Person" class
 #Create an attribute "name" in the "Person" class
 class Person:
-    def __init__(self,name):
+    def __init__(self,name,age):
         self.name = name
+        self.age = age
+    def info(self):
+        return f'Name:{self.name}, Age:{self.age}'
         
